c21b5b02fb8bc973f8e8284a327cfa60078f0395
Mahesh Asabe <mahesh.asabe@in-it0544.globant.com>
2018-05-17 12:13:19 +0530

    master : intial frameworl setup

---
+++ swiftutils/swiftutils/TestClass.swift
@@ -0,0 +1,17 @@
+//
+//  TestClass.swift
+//  swiftutils
+//
+//  Created by Mahesh Asabe on 17/05/18.
+//  Copyright © 2018 Mahesh Asabe. All rights reserved.
+//
+
+import Foundation
+
+public class TestClass {
+    
+    public static func getFrameworkName() -> String {
+        return "swiftutils"
+    }
+    
+}
---
+++ swiftutils/swiftutils/swiftutils.h
@@ -0,0 +1,19 @@
+//
+//  swiftutils.h
+//  swiftutils
+//
+//  Created by Mahesh Asabe on 17/05/18.
+//  Copyright © 2018 Mahesh Asabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swiftutils.
+FOUNDATION_EXPORT double swiftutilsVersionNumber;
+
+//! Project version string for swiftutils.
+FOUNDATION_EXPORT const unsigned char swiftutilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftutils/PublicHeader.h>
+
+
